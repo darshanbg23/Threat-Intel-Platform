febce3ebff15206e2a407bc2987ca17b48034873
Darshan BG <darshan.bg.23@gmail.com>
2026-05-02 21:14:32 +0530

    Updated Week3 firewall.py

--- Week3/firewall.py
+++ Week3/firewall.py
@@ -1,10 +1,12 @@
-def apply_rule():
-    pass
+import os
+
+SAFE_MODE = True
 
 
-def block_threat():
-    pass
-
-
-def allow_traffic():
-    pass
+def block_ip(ip):
+    if SAFE_MODE:
+        print(f"Simulated block for IP: {ip}")
+    else:
+        os.system(f"iptables -A INPUT -s {ip} -j DROP")
+    
+    print(f"Blocked IP: {ip}")
