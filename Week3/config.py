c6a38bac642bc1615efbe57b76fa7d71b2bb8c65
Darshan BG <darshan.bg.23@gmail.com>
2026-05-03 21:22:17 +0530

    Updated Week3 config.py with MAX_RUNS

--- Week3/config.py
+++ Week3/config.py
@@ -3,7 +3,9 @@
 MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "threat_db"
 COLLECTION_NAME = "indicators"
+
 RISK_THRESHOLD = "HIGH"
 CONFIDENCE_THRESHOLD = 70
-SLEEP_TIME = 10
 
+SLEEP_TIME = 2
+MAX_RUNS = 5
